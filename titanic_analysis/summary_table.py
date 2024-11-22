e757f521dde97fbb58a3536a648ca33d0200f87e
fzf24vyt <159143160+fzf24vyt@users.noreply.github.com>
2024-11-23 01:39:58 +0500

    summary_table.py

--- titanic_analysis/summary_table.py
+++ titanic_analysis/summary_table.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def create_summary_table(df):
     """
-    Creates a summary DataFrame with feature name, data type, number of unique values, and if it has missing values.
+    Creates a summary DataFrame with feature name, data type,  number of unique values, and if it has missing values.
 
     Args:
         df (pd.DataFrame): The Titanic dataset as a DataFrame.
